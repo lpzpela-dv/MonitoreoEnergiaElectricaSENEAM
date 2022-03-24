e3148bdb30b9a8c78e314283f6a6a3e2e3f38a83
pablolopezar <pabloe.lopezarmenta@outlook.com>
2022-03-23 17:02:52 -0700

    [add] Grafiuca para diesel dash general

--- public/js/dgeneral.js
+++ public/js/dgeneral.js
@@ -7,11 +7,27 @@ $(document).ready(function () {
         console.log($.cookie('id_aero_selected'));
         ModalSelectAero.hide();
         getStatusArea();
+        setupDieselGraph();
     });
 
 });
 
 
+var myChartDiesel = setupNewGraph('myChartDiesel', 'bar', 'Diesel por Planta');
+
+async function setupDieselGraph() {
+    let res = await fetch('http://localhost:8080/MonitoreoEnergiaElectricaSENEAM/public/api/status/area/lst/' + $.cookie('id_aero_selected')).then(response => response.json());
+    let i = 0;
+    let litros = [1500, 1000, 1850, 950, 1700];
+    let color = ['#ffc107', '#dc3545', '#198754', '#dc3545', '#198754',]
+    res.forEach(value => {
+        myChartDiesel.data.labels.push(value.areaName);
+        myChartDiesel.data.datasets[0].data.push(litros[i]);
+        myChartDiesel.data.datasets[0].backgroundColor.push(color[i]);
+        i += 1;
+    });
+    myChartDiesel.update();
+}
 async function getStatusArea() {
     let resp = await fetch('http://localhost:8080/MonitoreoEnergiaElectricaSENEAM/public/api/status/area/lst/' + $.cookie('id_aero_selected')).then(response => response.json());
     let vhtml = '';
@@ -27,21 +43,12 @@ async function getStatusArea() {
         } else {
             if (parseFloat(area.VoltL7) <= 117 || parseFloat(area.VoltL8) <= 117 || parseFloat(area.VoltL9) <= 117) {
                 color = 'danger';
-                // v1 = area.VoltL7;
-                // v2 = area.VoltL8;
-                // v3 = area.VoltL9;
             } else {
                 if (parseFloat(area.VoltL4) > 117 || parseFloat(area.VoltL5) > 117 || parseFloat(area.VoltL6) > 117) {
                     color = 'warning';
-                    // v1 = area.VoltL4;
-                    // v2 = area.VoltL5;
-                    // v3 = area.VoltL6;
                 } else {
                     if (parseFloat(area.VoltL1) > 117 || parseFloat(area.VoltL2) > 117 || parseFloat(area.VoltL3) > 117) {
                         color = 'success';
-                        // v1 = area.VoltL1;
-                        // v2 = area.VoltL2;
-                        // v3 = area.VoltL3;
                     }
                 }
             }
@@ -61,7 +68,55 @@ async function getStatusArea() {
     // }
 }
 
+function setupNewGraph(ctx, GType, GText) {
+    const _ctx = document.getElementById(ctx);
+    const config = {
+        type: GType,
+        data: {
+            datasets: [{
+                label: "Litros",
+                borderWidth: 3,
+                fill: false,
+                // borderColor: '#0275D8',
+                backgroundColor: [],
+                tension: 0.2
+            }]
+        },
+        options: {
+            responsive: true,
+            interaction: {
+                mode: 'index',
+                intersect: false,
+            },
+            stacked: false,
+            scales: {
+                y: {
+                    type: 'linear',
+                    display: true,
+                    position: 'left',
+                    grace: 2
+
+                }
+            },
+            plugins: {
+                title: {
+                    display: true,
+                    text: GText,
+                    color: "#000000"
+                },
+                legend: {
+                    labels: {
+                        boxWidth: 0
+                    }
+                }
+            }
+        }
+    }
+    const myChartTMP = new Chart(_ctx, config);
+    return myChartTMP;
+}
+
 setInterval(() => {
     console.log("Actualizando data");
     getStatusArea();
-}, 5000);
+}, 3000);
--- resources/views/dashboardGeneral.blade.php
+++ resources/views/dashboardGeneral.blade.php
@@ -28,123 +28,108 @@
                     <p class="h4 text-center">Estatus de Areas</p>
                 </tr>
                 <tr id="showAreas">
-
-                    {{-- <td class="text-center">
-                        <button type="button" class=" btn btn-sm btn-success rounded-pill"
-                            style="width: 5rem; height:5rem;">CCAR1</button>
-                    </td>
-                    <td class="text-center">
-                        <button type="button" class=" btn btn-sm btn-danger rounded-pill"
-                            style="width: 5rem; height:5rem;">CCAR2</button>
-                    </td>
-                    <td class="text-center">
-                        <button type="button" class=" btn btn-sm btn-warning rounded-pill"
-                            style="width: 5rem; height:5rem;">VOR</button>
-                    </td>
-                    <td class="text-center">
-                        <button type="button" class=" btn btn-sm btn-danger rounded-pill"
-                            style="width: 5rem; height:5rem;">RADAR</button>
-                    </td>
-                    <td class="text-center">
-                        <button type="button" class=" btn btn-sm btn-danger rounded-pill"
-                            style="width: 5rem; height:5rem;">TERRENA</button>
-                    </td> --}}
                 </tr>
             </tbody>
         </table>
     </div>
-    <div class="row justify-content-center">
-        <table class="table table-striped table-hover">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col" class="text-center">Log de eventos</th>
-                    <th scope="col">Fecha</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">2</th>
-                    <td class="text-center">Carga de sitio CCRA2 APAGADO</td>
-                    <td>15/03/2022 12:22:00</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td class="text-center">Carga de sitio CCRA2 ENCENDIDO</td>
-                    <td>15/03/2022 12:22:00</td>
-                </tr>
-                <tr>
-                    <th scope="row">4</th>
-                    <td class="text-center">Carga de sitio TERRENA APAGADO</td>
-                    <td>15/03/2022 12:22:00</td>
-                </tr>
-                <tr>
-                    <th scope="row">5</th>
-                    <td class="text-center">Carga de sitio CCRA2 APAGADO</td>
-                    <td>15/03/2022 12:22:00</td>
-                </tr>
-                <tr>
-                    <th scope="row">6</th>
-                    <td class="text-center">Carga de sitio CCRA2 APAGADO</td>
-                    <td>15/03/2022 12:22:00</td>
-                </tr>
-                <tr>
-                    <th scope="row">7</th>
-                    <td class="text-center">Carga de sitio CCRA2 APAGADO</td>
-                    <td>15/03/2022 12:22:00</td>
-                </tr>
-                <tr>
-                    <th scope="row">8</th>
-                    <td class="text-center">Carga de sitio CCRA2 APAGADO</td>
-                    <td>15/03/2022 12:22:00</td>
-                </tr>
+    {{-- <div class="row justify-content-center"> --}}
+        <div class="row">
+            <div class="col-md-6">
+                <canvas id="myChartDiesel" height="110hv"></canvas>
+            </div>
+            <div class="col">
+                <table class="table table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col" class="text-center">Log de eventos</th>
+                            <th scope="col">Fecha</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td class="text-center">Carga de sitio CCRA2 APAGADO</td>
+                            <td>15/03/2022 12:22:00</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td class="text-center">Carga de sitio CCRA2 ENCENDIDO</td>
+                            <td>15/03/2022 12:22:00</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">4</th>
+                            <td class="text-center">Carga de sitio TERRENA APAGADO</td>
+                            <td>15/03/2022 12:22:00</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">5</th>
+                            <td class="text-center">Carga de sitio CCRA2 APAGADO</td>
+                            <td>15/03/2022 12:22:00</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">6</th>
+                            <td class="text-center">Carga de sitio CCRA2 APAGADO</td>
+                            <td>15/03/2022 12:22:00</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">7</th>
+                            <td class="text-center">Carga de sitio CCRA2 APAGADO</td>
+                            <td>15/03/2022 12:22:00</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">8</th>
+                            <td class="text-center">Carga de sitio CCRA2 APAGADO</td>
+                            <td>15/03/2022 12:22:00</td>
+                        </tr>
 
-            </tbody>
-        </table>
-    </div>
+                    </tbody>
+                </table>
+            </div>
+        </div>
 
-    {{-- modal para seleccionar aeropuerto --}}
-    <div class="modal fade" id="selecAero" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Aeropuerto:</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form>
-                        <div class="mb-3">
-                            <select id="selectAero" class="form-select">
+        {{-- modal para seleccionar aeropuerto --}}
+        <div class="modal fade" id="selecAero" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Aeropuerto:</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                            <div class="mb-3">
+                                <select id="selectAero" class="form-select">
 
-                                @foreach ($aeropuertos as $aeropuerto)
-                                <option value="{{ $aeropuerto['id'] }}" selected>{{ $aeropuerto['shortName'] }} -
-                                    {{
-                                    $aeropuerto['description'] }}</option>
-                                @endforeach
+                                    @foreach ($aeropuertos as $aeropuerto)
+                                    <option value="{{ $aeropuerto['id'] }}" selected>{{ $aeropuerto['shortName'] }} -
+                                        {{
+                                        $aeropuerto['description'] }}</option>
+                                    @endforeach
 
-                            </select>
-                        </div>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" id="btnAero" class="btn btn-primary">Aceptar</button>
+                                </select>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" id="btnAero" class="btn btn-primary">Aceptar</button>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
-@section('js')
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/chart.js@3.7.0/dist/chart.min.js"></script>
-<script src="https://kit.fontawesome.com/c5bf36bb97.js" crossorigin="anonymous"></script>
-<script src="js/dgeneral.js"></script>
-<script src="js/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
-    integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
-</script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"
-    integrity="sha512-3j3VU6WC5rPQB4Ld1jnLV7Kd5xr+cq9avvhwqzbH/taCRNURoeEpoPBK9pDyeukwSxwRPJ8fDgvYXd6SkaZ2TA=="
-    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-{{-- <script src="js/graph.js"></script> --}}
-@endsection
+    @endsection
+    @section('js')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.7.0/dist/chart.min.js"></script>
+    <script src="https://kit.fontawesome.com/c5bf36bb97.js" crossorigin="anonymous"></script>
+    <script src="js/dgeneral.js"></script>
+    {{-- <script src="js/popper.min.js"></script> --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"
+        integrity="sha512-3j3VU6WC5rPQB4Ld1jnLV7Kd5xr+cq9avvhwqzbH/taCRNURoeEpoPBK9pDyeukwSxwRPJ8fDgvYXd6SkaZ2TA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    {{-- <script src="js/graph.js"></script> --}}
+    @endsection
